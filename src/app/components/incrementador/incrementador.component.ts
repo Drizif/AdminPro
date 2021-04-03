8e755d67f75ccd87491ccf559d8f6ca69dd120f9
Jose Rodriguez <joseluisrdzs@live.com>
2021-04-03 00:00:52 -0600

    @Output - Componente incrementador

--- src/app/components/incrementador/incrementador.component.ts
+++ src/app/components/incrementador/incrementador.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-incrementador',
@@ -8,15 +8,20 @@ import { Component, Input } from '@angular/core';
 })
 export class IncrementadorComponent {
   @Input() progreso: number = 0;
+  @Output() valorSalida: EventEmitter<number> = new EventEmitter();
 
   cambiarValor(valor: number) {
     if (this.progreso >= 100 && valor >= 0) {
+      this.valorSalida.emit(100);
       return this.progreso = 100;
     }
     if (this.progreso <= 0 && valor < 0) {
+      this.valorSalida.emit(0);
       return this.progreso = 0;
     }
+
     this.progreso += valor;
+    this.valorSalida.emit(this.progreso);
   }
 
 }
