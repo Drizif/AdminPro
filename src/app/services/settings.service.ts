1f59c9a19cf2ac3ea1d8207b7383c51ccb2de80d
Jose Rodriguez <joseluisrdzs@live.com>
2021-07-10 20:13:45 -0500

    settingsService

---
+++ src/app/services/settings.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SettingsService {
+  private linkTheme = document.querySelector('#theme');
+
+  constructor() {
+    console.log('Settings service init');
+    const url = localStorage.getItem('theme') || './assets/css/colors/default-dark.css';
+    this.linkTheme.setAttribute('href', url);
+    localStorage.setItem('theme', url);
+  }
+
+  changeTheme(theme: string) {
+    const url = `./assets/css/colors/${theme}.css`
+    this.linkTheme.setAttribute('href', url);
+    localStorage.setItem('theme', url);
+    
+    this.checkCurrentTheme();
+  }
+
+  checkCurrentTheme() {
+    const links: NodeListOf<Element> = document.querySelectorAll('.selector');;
+
+    links.forEach(e => {
+      e.classList.remove('working');
+      const btnTheme = e.getAttribute('data-theme');
+      const btnThemeUrl = `./assets/css/colors/${btnTheme}.css`
+      const currentTheme = this.linkTheme.getAttribute('href');
+
+      if (btnThemeUrl === currentTheme) {
+        e.classList.add('working');
+      }
+    });
+  }
+
+}
